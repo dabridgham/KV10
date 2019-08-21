8f309589146291a6f86875f292c56058209328fb
David Bridgham <dab@froghouse.org>
2019-08-21 14:46:40 -0400

    Removed the ifdef IACK; I'm not going back to sending the acks one cycle early.

--- mem-simple.sv
+++ mem-simple.sv
@@ -1,6 +1,6 @@
 //	-*- mode: Verilog; fill-column: 90 -*-
 //
-// Hacked up memory, really simplified, for testing inside Xilinx
+// Hacked up memory, really simplified, for testing inside Xilinx Vivado
 
 `timescale 1 ns / 1 ns
 
@@ -20,36 +20,24 @@ module mem
    );
 
 //   reg [`WORD] 	      ram[0:2**`PADDRSIZE-1];
-   reg [`WORD] 	      ram[0:2**14-1]; // fit into the Block RAM in the FPGA
+   reg [`WORD] 	      ram[0:2**15-1]; // fit into the Block RAM in the FPGA
 
    initial begin
       $readmemh("dgcaa.mif", ram);
    end
 
-`ifdef IACK
-   // push the acks immediately so they're there a cycle earlier than the data.
-   always @(*) read_ack = mem_read;
-   always @(*) write_ack = mem_write;
-`endif
-
    always @(posedge clk) begin
-`ifndef IACK
       read_ack <= 0;
       write_ack <= 0;
-`endif
 
       if (mem_read) begin
 	 mem_read_data <= ram[mem_addr];
-`ifndef IACK
 	 read_ack <= 1;
-`endif
       end
 
       if (mem_write) begin
 	 ram[mem_addr] <= mem_write_data;
-`ifndef IACK
 	 write_ack <= 1;
-`endif
       end
    end
 
